d7ca339511a96f1427b0edef744680d3356a54e0
Erich Keil <ekeil@tulsaopera.com>
2020-04-09 19:51:26 -0400

    Updates

---
+++ precache-manifest.aab57dbb33fb43a26b081c3e9a875cca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9a00f24d7cb86d2ca08dedc8fddfc88",
+    "url": "/randquote/index.html"
+  },
+  {
+    "revision": "0f5cd2b0404d73355b5b",
+    "url": "/randquote/static/css/main.591441bb.chunk.css"
+  },
+  {
+    "revision": "74f331fce48a9f80f637",
+    "url": "/randquote/static/js/2.c821275e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/randquote/static/js/2.c821275e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f5cd2b0404d73355b5b",
+    "url": "/randquote/static/js/main.3ad7d0c7.chunk.js"
+  },
+  {
+    "revision": "7ea9aeb7003ff7f8b051",
+    "url": "/randquote/static/js/runtime-main.de783383.js"
+  },
+  {
+    "revision": "eef9e137e1d0895d94dbe56cf9e76523",
+    "url": "/randquote/static/media/mb_text_bg.eef9e137.jpg"
+  }
+]);
