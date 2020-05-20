25a4da072c446d40709decf62cfeea693c6ec765
Erich Keil <39918654+zenlex@users.noreply.github.com>
2020-05-20 09:01:14 -0400

    fixed duplicated css id tweet-quote

--- src/randQuote.jsx
+++ src/randQuote.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './randQuote.css';
-import { CSSTransitionGroup }  from 'react-transition-group';
+import { CSSTransitionGroup } from 'react-transition-group';
 
 //Random Quote Generator - Free Code Camp Front End Libraries Certification - Project 1
 /****************************************************************************************
@@ -22,26 +22,26 @@ import { CSSTransitionGroup }  from 'react-transition-group';
 
 
 const QAPI = "https://type.fit/api/quotes";
-let quotesArr =[];
+let quotesArr = [];
 
-class QuoteGen extends React.Component{
-    constructor(props){
+class QuoteGen extends React.Component {
+    constructor(props) {
         super(props);
         this.state = {
-            quote:'', 
+            quote: '',
             author: '',
             quoteindex: 0
         }
         this.clickHandle = this.clickHandle.bind(this);
     }
-    
-    setQuote(){
-         /*Pick random index from array of quotes and set state */
-        
-         let index = Math.floor(Math.random()*quotesArr.length)
-        this.setState({quote:quotesArr[index].text, author:quotesArr[index].author, quoteindex:index});
-        if(quotesArr[index].author === null){
-            this.setState({author:"Anonymous"});
+
+    setQuote() {
+        /*Pick random index from array of quotes and set state */
+
+        let index = Math.floor(Math.random() * quotesArr.length)
+        this.setState({ quote: quotesArr[index].text, author: quotesArr[index].author, quoteindex: index });
+        if (quotesArr[index].author === null) {
+            this.setState({ author: "Anonymous" });
         }
     }
 
@@ -51,51 +51,51 @@ class QuoteGen extends React.Component{
             const response = await fetch(QAPI);
             quotesArr = await response.json();
             await this.setQuote();
-        } catch(err) {
+        } catch (err) {
             alert(err);
         }
-    }   
+    }
 
-    componentDidUpdate(){
-        
+    componentDidUpdate() {
+
     }
 
     clickHandle(event) {
-        this.setQuote();  
+        this.setQuote();
     };
 
 
-        
+
     render() {
-        return(
-                <div id='quote-box'>
-                    <CSSTransitionGroup
+        return (
+            <div id='quote-box'>
+                <CSSTransitionGroup
                     transitionName="fadeboth"
                     transitionEnterTimeout={1000}
                     transitionLeaveTimeout={0}
                     transitionLeave={false}
-                    >
-                    <div  id='text' key={this.state.quoteindex} className='fontDefs'>"{this.state.quote}"</div>
-                    <div  id='author' key={this.state.quoteindex + 1} className='fontDefs'>-{this.state.author}</div> 
-                    </CSSTransitionGroup>
-                    <input id='new-quote' className='fontDefs' type="submit" value="Get New Quote" onClick={this.clickHandle} />
-                    <TweetIt quote={encodeURIComponent(this.state.quote)} author={this.state.author}/>  
-                </div>
-            )
+                >
+                    <div id='text' key={this.state.quoteindex} className='fontDefs'>"{this.state.quote}"</div>
+                    <div id='author' key={this.state.quoteindex + 1} className='fontDefs'>-{this.state.author}</div>
+                </CSSTransitionGroup>
+                <input id='new-quote' className='fontDefs' type="submit" value="Get New Quote" onClick={this.clickHandle} />
+                <TweetIt quote={encodeURIComponent(this.state.quote)} author={this.state.author} />
+            </div>
+        )
 
-    }    
+    }
 }
-    
-    export default QuoteGen;
 
-function TweetIt(props){
-    return(
+export default QuoteGen;
+
+function TweetIt(props) {
+    return (
         <div className='icoButton'>
-            <a id='tweet-quote' href = 
-            {`https://twitter.com/intent/tweet?text="${String(props.quote)}"%0a-${props.author}`} target="_blank" rel="noopener noreferrer">
+            <a id='tweet-quote' href=
+                {`https://twitter.com/intent/tweet?text="${String(props.quote)}"%0a-${props.author}`} target="_blank" rel="noopener noreferrer">
                 <img id="twitter-logo" src="./assets/twitter_bird.png" alt="Twitter Logo"></img>
-                <span className='fontDefs' id='tweet-quote'>Tweet It!</span>
+                <span className='fontDefs'>Tweet It!</span>
             </a>
-        </div> 
+        </div>
     )
 }
